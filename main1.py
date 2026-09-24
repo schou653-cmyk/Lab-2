631107272e0659c773b9127914d5a61a87fdbda4
saki chou <schou653@mtroyal.ca>
2026-09-23 20:31:20 -0600

    finished step 1 and 2

--- main1.py
+++ main1.py
@@ -13,17 +13,54 @@ textA = []
 
 with open("textA.txt", "r") as file:
 
-    for linee in file:
-        words = line.split()
+    for line in file:
+        words = line.lower().split()
 
         for word in words:
+            word = word.strip(".,!?'")
             if word not in stopwords:
                 textA.append(word)
 
-print(textA)
+
+
+textB = []
+
+with open("textB.txt","r") as file:
+    for line in file:
+        words = line.lower().split()
+
+        for word in words:
+            word =  word.strip(",.!?'")
+            if word not in stopwords:
+                textB.append(word)
+
+
+textA_counts = {}
+
+for word in textA:
+    if word in textA_counts:
+        textA_counts[word] += 1
+    else:
+        textA_counts[word] = 1
 
 
 
+for word in textA_counts:
+    textA_counts[word] = textA_counts[word] / len(textA)
+
+
+
+textB_counts = {}
+
+for word in textB:
+    if word in textB_counts:
+        textB_counts[word] += 1
+    else:
+        textB_counts[word] = 1
+
+
+for word in textB_counts:
+    textB_counts[word] = textB_counts[word]/ len(textB)
 
 
 
