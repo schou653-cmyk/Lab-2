7c06e00e800a663d81759fdbb82ef4ecceb2bb70
saki chou <schou653@mtroyal.ca>
2026-09-23 19:55:27 -0600

    cool

--- main1.py
+++ main1.py
@@ -1,5 +1,33 @@
 #What's up cool guy -saki
 
+stopwords = set()
+
+with open("stopwords.txt", "r") as file:
+
+    for line in file:
+        word = line.strip()
+        stopwords.add(word)
+
+
+textA = []
+
+with open("textA.txt", "r") as file:
+
+    for linee in file:
+        words = line.split()
+
+        for word in words:
+            if word not in stopwords:
+                textA.append(word)
+
+print(textA)
+
+
+
+
+
+
+
 
 # This is the main program
 # write your program here
